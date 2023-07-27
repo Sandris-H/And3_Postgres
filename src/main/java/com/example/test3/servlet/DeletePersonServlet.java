07d0f511c82fb2968bf40d8f15808d8a12110b88
Aliaksandr Hanchar <ahanchar808@gmail.com>
2023-07-27 14:06:58 +0200

    9

--- src/main/java/com/example/test3/servlet/DeletePersonServlet.java
+++ src/main/java/com/example/test3/servlet/DeletePersonServlet.java
@@ -9,16 +9,12 @@ import java.io.IOException;
 
 @WebServlet(name = "DeletePersonServlet", value = "/delete")
 public class DeletePersonServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PersonDAO personDAO = new PersonDAO();
         final int id = Integer.parseInt(request.getParameter("id"));
         personDAO.delete(id);
-        response.sendRedirect("/");
+        response.sendRedirect(request.getContextPath() + "/");
     }
 }
